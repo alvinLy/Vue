3b8b76ecb2bf0320a5f0add03c0d2d5859cf86d8
vinhan <nguyen_ly_vinh_an@allgrow-labo.jp>
2020-03-05 17:07:41 +0700

    no message

--- study/js/directive/script.js
+++ study/js/directive/script.js
@@ -9,6 +9,23 @@ Vue.directive('swap-color', function(el, binding){
 	}, 500)
 })
 
+Vue.directive('dropdown-menu', {
+	bind(button, b, vnode, oldVnode) {
+		console.log(b);
+	   	const id = b.value
+		button.addEventListener('click', (b) => {
+			var elsToToggle = document.querySelectorAll(`[hide-name="${id}"]`);
+			console.log(button, b.value, `[hide-name="${id}"]`, elsToToggle);
+			console.log(button, " clicked");
+			elsToToggle.forEach((el) => {
+				console.log(el);
+				el.classList.toggle('show');
+			})
+		}, false)
+	}
+});
+
+
 new Vue({
 	el:'#app',
 	data: {}
--- todo-vue/src/store/store.js
+++ todo-vue/src/store/store.js
@@ -22,25 +22,25 @@ export const store = new Vuex.Store({
    	]		
 	},
 	getters: {
-   	remaining(state){
-   		return state.todos.filter(item => !item.completed).length
-   	},
-   	anyRemaining(state, getters){
-   		return getters.remaining != 0
-   	},
-   	todosFilter(state){
-   		if (state.filter == 'all') {
-   			return state.todos
-   		} else if (state.filter == 'active') {
-   			return state.todos.filter(item => !item.completed)
-   		} else if (state.filter == 'completed') {
-				return state.todos.filter(item => item.completed)
-			}
+		remaining(state){
+			return state.todos.filter(item => !item.completed).length
+		},
+		anyRemaining(state, getters){
+			return getters.remaining != 0
+		},
+		todosFilter(state){
+			if (state.filter == 'all') {
+				return state.todos
+			} else if (state.filter == 'active') {
+				return state.todos.filter(item => !item.completed)
+			} else if (state.filter == 'completed') {
+					return state.todos.filter(item => item.completed)
+				}
 
-   		return state.todos
-   	},
-   	showClearCompletedButton(state){
-   		return state.todos.filter(item => item.completed).length > 0
-   	}		
+			return state.todos
+		},
+		showClearCompletedButton(state){
+			return state.todos.filter(item => item.completed).length > 0
+		}		
 	}
 })
